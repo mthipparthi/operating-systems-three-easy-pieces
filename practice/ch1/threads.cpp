512dae3ada6474baf3e6d8fd3e60451d7bf17d13
Mahesh Thipparthi <mahesh.thipparthi@isentia.com>
2019-12-30 18:51:27 +1100

    Chapter 1'

---
+++ practice/ch1/threads.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "common.h"
+#include "common_threads.h"
+
+pthread_mutex_t mymutex = PTHREAD_MUTEX_INITIALIZER;
+
+
+
+volatile int counter = 0; 
+int loops;
+
+void *worker(void *arg) {
+    int i;
+    for (i = 0; i < loops; i++) {
+        pthread_mutex_lock(&mymutex);
+	    counter++;
+        pthread_mutex_unlock(&mymutex);
+    }
+    return NULL;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) { 
+	fprintf(stderr, "usage: threads <loops>\n"); 
+	exit(1); 
+    } 
+    loops = atoi(argv[1]);
+    pthread_t p1, p2;
+    printf("Initial value : %d\n", counter);
+    Pthread_create(&p1, NULL, worker, NULL); 
+    Pthread_create(&p2, NULL, worker, NULL);
+    Pthread_join(p1, NULL);
+    Pthread_join(p2, NULL);
+    printf("Final value   : %d\n", counter);
+    return 0;
+}
